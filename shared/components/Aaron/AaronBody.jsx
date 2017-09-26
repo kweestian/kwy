760b982c82a032d257084e830897f9296ef887df
kweestian <chrislhamelin@gmail.com>
2017-09-25 18:10:57 -0700

    changes to aaron

--- shared/components/Aaron/AaronBody.jsx
+++ shared/components/Aaron/AaronBody.jsx
@@ -12,36 +12,21 @@ class AaronBody extends Component {
       <section className="container content-section text-center">
         <div className="row">
           <div className="col-lg-8 col-lg-offset-2">
-            <h2>The Lukas Legacy & The Aaron Project</h2>
+            <h2> The Lukas Legacy & The Aaron Project</h2>
             <img src="/img/aaron-profile.jpg" className="lukas-container__image"  />
-            <p>
-              On September 5th, 2016 Lukas Oelke passed away, ending what we called The Lukas Project.  Our fight was over, but we made one last promise to our friend: to keep his memory alive in the help we provide to other young people with cancer.  In this effort we make sure Lukas’ death was not in vain.  <b>We call it The Lukas Legacy.</b>
-            </p>
-            <p>
-              The first recipient of The Lukas Legacy is Aaron Konkin.  Aaron is similar to Lukas in a lot of ways. For one, he was diagnosed in a similar place in life at the age of 24. He had recently graduated from UVic with a degree in computer science and applied ethics. He was fit, spreading his workout routine between the gym and various Martial Arts disciplines. Aaron also found himself a new girlfriend, whom he liked a lot. The universe, it appeared, was on his side.
-            </p>
-            <p>
-              All of that came crashing down in February of 2016, when a persistent dry cough led to a diagnosis of a rare type of cancer called Alveolar Soft Part Sarcoma.
-            </p>
-            <p>
-              Due to complications from his disease Aaron has been forced into unemployment. He has no extended medical coverage as his family plan ceased upon his graduation. In addition, his treatment regime requires many supplements, which cost roughly $150 a day. On top of that, Aaron’s father and step-mother have less time to work and must now routinely incur the travel costs between Vancouver and Vernon.  Moreover, they do not have the financial means to cover over $5000 each month in Aarons’ living expenses.
-            </p>
+            <div className="paragraph-text">
+              <p>It is with a heavy heart that we announce the peaceful passing of Aaron Konkin on September 16th, 2017 surrounded by his friends and family. </p>
 
-            <p>
-              Sadly, this isn’t the first-time Aaron’s family has fallen upon financial hardship. Several years ago, for instance, Aaron’s parents almost lost everything after real-estate investments turned sour due to a market collapse. Aaron’s father, Kenn, spent 18 months out of Province working disaster relief to bring his family back from the brink of financial ruin.  That is, until they received the news that their son was diagnosed with cancer.
-            </p>
-            <p>
-              Once again, Aaron’s family has found themselves in a tough financial situation, but this time their son’s life is also at stake. Aaron’s father has had to drop all his employment responsibilities and is now staying in Vancouver full time to act as the primary care giver of his son.
-            </p>
-            <p>
-              Kids Without Yachts is here to keep Aaron’s boat afloat. But we can’t do it without your support. Many of you rallied behind Lukas when he was diagnosed and we are asking you to give that same love and support to Aaron. As always, any donations made through our website are directed to our current “Project” AKA Aaron.  No KWY member receives ANY salary, or monetary benefit from donations received.
-            </p>
-            <p>
-              We’ve also recently included an option to be a <Link to="/donate" className="text-default"> monthly donor to Kids Without Yachts.</Link> This is by far the most effective way to support Aaron. It allows us to budget, plan ahead, keep our administration costs lower, and organize ourselves in the most efficient way possible.
-            </p>
-            <p>
-              In addition to donating now, please join us March 29th at the Local Public Eatery just outside of Kits Beach on Cornwall. We have a reputation to uphold and we promise it’ll be a ton of fun ;)
-            </p>
+              <p>Aaron was not just an inspiration to his friends and family, he made a lasting impression on the Kids Without Yachts team. He was dedicated to his fight, but also felt strongly about keeping a sense of normalcy.  The first time we hung out with Aaron we went clay shooting in Squamish. The second time we tossed a frisbee in the park. Here we had a young adult living with stage 4 Alveolar Soft Part Sarcoma who made it a priority to be doing great outdoor activities with his friends, just like any of us would. </p>
+
+              <p>This was something we could strongly relate to from personal experience. We believe young adults living with cancer should always have time to be a <span className="text-default"> Kid.</span> </p>
+
+              <p>Aaron had many hurdles thrown at him: he took each one in stride and looked on for the next challenge. Ultimately, Aaron succumbed to his disease, but his undying dedication to getting better remains a beacon of hope to all the other Kids out there.</p>
+
+              <p>The Kids Without Yachts organization, which includes <span className="text-default" >you</span>, were able to raise <span className="text-large">$12, 800</span> that went directly into Aaron’s pockets.</p>
+
+              <p>On a personal note, we would all like to thank Brenda, Ken, Anthony and Michelle, and all others who were of paramount importance in helping Aaron. </p>
+            </div>
           </div>
         </div>
         <Donate />
--- shared/components/Lukas/LukasBody.jsx
+++ shared/components/Lukas/LukasBody.jsx
@@ -18,39 +18,41 @@ class LukasBody extends Component {
           <div className="col-lg-8 col-lg-offset-2">
             <h2>How We've Helped</h2>
             <img src="/img/lukas_studly.jpg" className="lukas-container__image"  />
-            <p>
-              With considerable sadness we advise that Lukas Oelke breathed his last in the early afternoon of September 5th, 2016. Lukas was incredible to the end, showing his love and acknowledging his family and friends with claps of his hands and gestures of thumbs up.
-            </p>
-            <p>
-            Although this was not the end we sought to achieve, we are mightily proud of The Lukas Project. In our minds The Lukas Project represents human compassion to it’s fullest extent. We are overblown by the support we’ve received. Never before have we seen such a large community come together to aide one of their own.
-            </p>
-            <p>
-            As always, every dollar received is geared towards providing young individuals with cancer the financial means to reap the rewards of both the public and private healthcare sectors. This money was donated by you. You should be able to see exactly how it was spent. Here it is:
-            </p>
-            <p>
-              <h4>$55,177 has been invested into The Lukas Project. Of that:</h4>
-              <p>$30,807 went directly into the pockets of Lukas’ family for living costs</p>
-              <p>$10,500 was given to the family for rent towards a wheelchair accessible apartment</p>
-              <p>$8,494 was spent on cremation & funeral services</p>
-              <p>$3,376 was spent on a specialized custom wheelchair</p>
-              <p>$1,000 was spent on a mechanical bed</p>
-              <p> $1000 miscellaneous  </p>
-            </p>
-            <p>
-              Outside of the above, any funds received has gone towards event management, merchandise, and minor operating costs.
-            </p>
-            <p>
-              On a personal note, this experience has made all of us all too keenly aware of the devastating consequences of cancer. The grief we feel for our fallen brother is inexplicable.
-            </p>
-            <p>
-              We can think of but one way to honour Lukas: One day, within our lifetime, a young person with cancer – the next “Lukas” – will be diagnosed. And he will make it.
-            </p>
-            <p>
-              We invite every one of you who invested yourself in Lukas to refocus your energy towards achieving this goal. The Lukas Project may be over, but we’re not stopping. We're hoping you won’t either.
-            </p>
-            <p>
-              In the words of <a className="text-default" href="http://www.martinamisweb.com/" target="_blank">Martin Amis</a>, "This is where we really go when we die: into the hearts of those who remember us".
-            </p>
+              <div className="paragraph-text">
+                <p>
+                  With considerable sadness we advise that Lukas Oelke breathed his last in the early afternoon of September 5th, 2016. Lukas was incredible to the end, showing his love and acknowledging his family and friends with claps of his hands and gestures of thumbs up.
+                </p>
+                <p>
+                Although this was not the end we sought to achieve, we are mightily proud of The Lukas Project. In our minds The Lukas Project represents human compassion to it’s fullest extent. We are overblown by the support we’ve received. Never before have we seen such a large community come together to aide one of their own.
+                </p>
+                <p>
+                As always, every dollar received is geared towards providing young individuals with cancer the financial means to reap the rewards of both the public and private healthcare sectors. This money was donated by you. You should be able to see exactly how it was spent. Here it is:
+                </p>
+                <p>
+                  <h2>$55,177 has been invested into The Lukas Project. Of that:</h2>
+                  <p>$30,807 went directly into the pockets of Lukas’ family for living costs</p>
+                  <p>$10,500 was given to the family for rent towards a wheelchair accessible apartment</p>
+                  <p>$8,494 was spent on cremation & funeral services</p>
+                  <p>$3,376 was spent on a specialized custom wheelchair</p>
+                  <p>$1,000 was spent on a mechanical bed</p>
+                  <p> $1000 miscellaneous  </p>
+                </p>
+                <p>
+                  Outside of the above, any funds received has gone towards event management, merchandise, and minor operating costs.
+                </p>
+                <p>
+                  On a personal note, this experience has made all of us all too keenly aware of the devastating consequences of cancer. The grief we feel for our fallen brother is inexplicable.
+                </p>
+                <p>
+                  We can think of but one way to honour Lukas: One day, within our lifetime, a young person with cancer – the next “Lukas” – will be diagnosed. And he will make it.
+                </p>
+                <p>
+                  We invite every one of you who invested yourself in Lukas to refocus your energy towards achieving this goal. The Lukas Project may be over, but we’re not stopping. We're hoping you won’t either.
+                </p>
+                <p>
+                  In the words of <a className="text-default" href="http://www.martinamisweb.com/" target="_blank">Martin Amis</a>, "This is where we really go when we die: into the hearts of those who remember us".
+                </p>
+              </div>
           </div>
         </div>
         <Donate />
